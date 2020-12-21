a8708501f712ca7614501e85b09231302eba29a4
Chowdhury Md. Rabby Mahmud <md.rabby.mahmud@gmail.com>
2020-12-21 17:23:41 +0600

    Get categories from database public image show command: php artisan storage:link

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -15,7 +15,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Category::get();
+        return view('admin.category.index',compact('categories'));
     }
 
     /**
