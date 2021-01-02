93d15b7b2046a4f380ca323ee6e60f572f376563
Chowdhury Md. Rabby Mahmud <md.rabby.mahmud@gmail.com>
2021-01-02 17:16:32 +0600

    Link: https://getbootstrap.com/docs/5.0/examples/album/ Link: https://getbootstrap.com/docs/4.0/components/carousel/ frontend design partt3

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,7 +3,32 @@
 @section('content')
     <div class="container">
         <main>
-
+            <div id="carouselExampleIndicators1" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="images/gaming.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="images/gaming.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="images/gaming.jpg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators1" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators1" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
             <section class="py-5 text-center container">
                 <div class="row py-lg-5">
                     <div class="col-lg-6 col-md-8 mx-auto">
@@ -16,7 +41,7 @@
                     </div>
                 </div>
             </section>
-            <h2>Category</h2>
+            <h2>Categories</h2>
             @foreach(App\Category::all() as $category)
                 <button class="btn btn-outline-secondary">{{$category->name}}</button>
             @endforeach
@@ -52,6 +77,52 @@
                             <h2>Product not yet ready</h2>
                         @endif
                     </div>
+                </div>
+            </div>
+            <div class="jumbotron">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="row">
+                                <div class="col-3">
+                                    <img class="img-thumbnail" src="images/gaming.jpg">
+                                </div>
+                                <div class="col-3">
+                                    <img class="img-thumbnail" src="images/gaming.jpg">
+                                </div>
+                                <div class="col-3">
+                                    <img class="img-thumbnail" src="images/gaming.jpg">
+                                </div>
+                                <div class="col-3">
+                                    <img class="img-thumbnail" src="images/gaming.jpg">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="row">
+                                <div class="col-3">
+                                    <img class="img-thumbnail" src="images/gaming.jpg">
+                                </div>
+                                <div class="col-3">
+                                    <img class="img-thumbnail" src="images/gaming.jpg">
+                                </div>
+                                <div class="col-3">
+                                    <img class="img-thumbnail" src="images/gaming.jpg">
+                                </div>
+                                <div class="col-3">
+                                    <img class="img-thumbnail" src="images/gaming.jpg">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
             </div>
 
