1ffb41e4134ea71c1fdcbb260fd548e0c93dce94
Chowdhury Md. Rabby Mahmud <md.rabby.mahmud@gmail.com>
2021-02-02 14:36:01 +0600

    last

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,6 +72,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{route('order')}}">Order</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
