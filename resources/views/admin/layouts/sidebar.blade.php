cef94fd231fcc2a41b6f1124aac083085d783cdd
Chowdhury Md. Rabby Mahmud <md.rabby.mahmud@gmail.com>
2021-02-07 20:54:46 +0600

    Last commit 172. source code

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,13 +1,13 @@
 <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{url('/auth/dashboard')}}">
         <div class="sidebar-brand-icon">
-            <img src="{{asset('admin/img/logo/logo2.png')}}">
+            <img src="{{asset('admin/img/logo/xobotronix.png')}}">
         </div>
-        <div class="sidebar-brand-text mx-3">RuangAdmin</div>
+        <div class="sidebar-brand-text mx-3">E-commerce-bd</div>
     </a>
     <hr class="sidebar-divider my-0">
     <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="{{url('/auth/dashboard')}}">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
     </li>
